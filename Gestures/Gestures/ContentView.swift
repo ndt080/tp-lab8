cb84d19788775fafdc7a3b78e5ace3389eb0a102
ndt080 <68751397+ndt080@users.noreply.github.com>
2021-04-21 16:48:35 +0300

    Add z1: Gestures

---
+++ Gestures/Gestures/ContentView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var didLongPress: Bool = false
+    @State private var scale: CGFloat = 1
+    @State var rotation: Angle = .zero
+    @State private var changeColor = Color.purple
+    
+    var body: some View {
+        
+        Rectangle()
+            .scaleEffect(scale)
+            .rotationEffect(rotation)
+            .foregroundColor(changeColor)
+            .frame(width: 210, height: 210, alignment: .center)
+            .shadow(radius: 10)
+            .animation(.default)
+            .simultaneousGesture(DragGesture()
+                                    .onEnded { value in
+                                        changeColor = value.location.x > 0 ? Color.gray : Color.black
+                                        changeColor = value.location.x < 0 ? Color.gray : Color.black
+                                    }
+            )
+            .simultaneousGesture(TapGesture()
+                                    .onEnded { _ in
+                                        self.changeColor = self.changeColor == Color.purple ? Color.blue : Color.purple
+                                    }
+            )
+            .simultaneousGesture(LongPressGesture()
+                                    .onEnded { _ in
+                                        self.scale = self.didLongPress == false ? 10 : 1
+                                        self.didLongPress.toggle()
+                                    }
+            )
+            .simultaneousGesture(RotationGesture()
+                                    .onChanged { value in
+                                        self.rotation = value
+                                    }
+            )
+            .simultaneousGesture(MagnificationGesture()
+                                    .onChanged { value in
+                                        self.scale = value
+                                    }
+            )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gestures/Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Andrei Petrov on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
