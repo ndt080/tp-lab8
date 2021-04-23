80572e8c9689ab2648caacbc70cb8b44a9b72f0a
ndt080 <68751397+ndt080@users.noreply.github.com>
2021-04-23 10:43:50 +0300

    update

---
+++ Paint/Paint/DrawingPad.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import CoreGraphics
+
+
+struct DrawingPad: View {
+    @Binding var currentDrawing: Drawing
+    @Binding var drawings: [Drawing]
+    @Binding var color: Color
+    @Binding var lineWidth: CGFloat
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for drawing in self.drawings {
+                    self.add(drawing: drawing, toPath: &path)
+                }
+                self.add(drawing: self.currentDrawing, toPath: &path)
+            }
+            .stroke(self.color, style: StrokeStyle(lineWidth: self.lineWidth, lineCap: .round, lineJoin: .round))
+            .background(Color.white)
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ (value) in
+                        let currentPoint = value.location
+                        if currentPoint.y >= 0
+                            && currentPoint.y < geometry.size.height {
+                            self.currentDrawing.points.append(currentPoint)
+                        }
+                    })
+                    .onEnded({ (value) in
+                        self.drawings.append(self.currentDrawing)
+                        self.currentDrawing = Drawing()
+                    })
+            )
+        }
+        .frame(maxHeight: .infinity)
+    }
+    
+    private func add(drawing: Drawing, toPath path: inout Path) {
+        let points = drawing.points
+        if points.count > 1 {
+            for i in 0..<points.count-1 {
+                let current = points[i]
+                let next = points[i+1]
+                path.move(to: current)
+                path.addLine(to: next)
+            }
+        }
+    }
+    
+}
+
+struct Drawing {
+    var points: [CGPoint] = [CGPoint]()
+}
